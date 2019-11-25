cb1845dae8fede49139a7dec5a8e241b81919f47
JackieTien97 <JackieTien@foxmail.com>
2019-11-25 18:39:24 +0800

    Chunk reader

---
+++ tsfile/src/test/java/org/apache/iotdb/tsfile/read/PageReaderTest.java
@@ -0,0 +1,90 @@
+package org.apache.iotdb.tsfile.read;
+
+import org.apache.iotdb.tsfile.read.common.Path;
+import org.apache.iotdb.tsfile.read.common.RowRecord;
+import org.apache.iotdb.tsfile.read.expression.IExpression;
+import org.apache.iotdb.tsfile.read.expression.QueryExpression;
+import org.apache.iotdb.tsfile.read.expression.impl.BinaryExpression;
+import org.apache.iotdb.tsfile.read.expression.impl.GlobalTimeExpression;
+import org.apache.iotdb.tsfile.read.filter.TimeFilter;
+import org.apache.iotdb.tsfile.read.query.dataset.QueryDataSet;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yuan Tian
+ */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class PageReaderTest {
+  private ReadOnlyTsFile readTsFile;
+  private ArrayList<Path> paths = new ArrayList<>();
+  private IExpression timeFilter;
+  private IExpression chunkFilter;
+
+//  @Param({"45", "1000", "10000", "100000", "500000"})
+//  private int bufferCapacity; // ensure that the file to read is large enough
+//  @Param({"99", "10000", "100000","300000", "1000000", "1500000", "2500000", "3000000", "3200000"})
+//  private long pos; // ensure that the file to read is large enough
+
+
+  @Setup(Level.Invocation) // please make sure you understand Level.Invocation
+  public void prepare() throws IOException {
+    String file = "/Users/jackietien/Desktop/IoTDB/查询/1574251382232-101-0.tsfile";
+    TsFileSequenceReader reader = new TsFileSequenceReader(file);
+    readTsFile = new ReadOnlyTsFile(reader);
+    paths.add(new Path("root.group_0.d_0.s_3"));
+    timeFilter = BinaryExpression.and(new GlobalTimeExpression(TimeFilter.gtEq(1605482800000L)),
+            new GlobalTimeExpression(TimeFilter.ltEq(1605482850000L)));
+    chunkFilter = BinaryExpression.and(new GlobalTimeExpression(TimeFilter.gtEq(1604482800000L)),
+            new GlobalTimeExpression(TimeFilter.ltEq(1671592795000L)));
+  }
+
+  @TearDown(Level.Invocation)
+  public void close() throws IOException {
+    readTsFile.close();
+  }
+
+//  @Benchmark
+//  public List<RowRecord> test10Points() throws IOException {
+//    QueryExpression queryExpression = QueryExpression.create(paths, timeFilter);
+//    QueryDataSet queryDataSet = readTsFile.query(queryExpression);
+//    List<RowRecord> res = new ArrayList<>();
+//    while (queryDataSet.hasNext()) {
+//      res.add(queryDataSet.next());
+//    }
+//    return res;
+//  }
+
+  @Benchmark
+  public List<RowRecord> testOneWholeChunk() throws IOException {
+    QueryExpression queryExpression = QueryExpression.create(paths, chunkFilter);
+    QueryDataSet queryDataSet = readTsFile.query(queryExpression);
+    List<RowRecord> res = new LinkedList<>();
+    while (queryDataSet.hasNext()) {
+      res.add(queryDataSet.next());
+    }
+    return res;
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+            .include(PageReaderTest.class.getSimpleName())
+            .forks(1)
+            .warmupIterations(5)
+            .measurementIterations(10)
+            .build();
+
+    new Runner(opt).run();
+  }
+}
